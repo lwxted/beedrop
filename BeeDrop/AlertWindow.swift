c821d6d9491b2e3f1145a8fc5d3e6587cebb033c
ROBIN ZHOU <robinzhou@ROBINs-MacBook-Pro.local>
2014-09-28 20:41:46 -0400

    alertwindow2

--- BeeDrop/AlertWindow.swift
+++ BeeDrop/AlertWindow.swift
@@ -35,7 +35,7 @@ class AlertWindow : NSObject, UIAlertViewDelegate {
             // Send request to server.
             var handeler = RequestHandler()
             handeler.sendRequestByURL(jsonObjectDriverConfirm, tag: "driverAcceptRequest")
-            sleep(5.0)
+            sleep(5)
             println("sent out confirm to \(self.mMatchedUserId)")
 
         })
@@ -68,14 +68,6 @@ class AlertWindow : NSObject, UIAlertViewDelegate {
         var mAttr: [String] = []
         for keyword in delKeyWords {
             var str: String
-            /*
-            if (keyword == "fromLoc" || keyword == "toLoc") {
-                let coords = info[keyword]! as [Double]
-                str = "( + \(coords[0]) + , + \(coords[1]) + )"
-            } else {
-                str = (info[keyword]! as? String)!
-            }
-            */
             println(keyword)
             /*
             if (keyword == "payment") {
@@ -107,32 +99,14 @@ class AlertWindow : NSObject, UIAlertViewDelegate {
     }
     
     func driverAcceptDataHelper() -> [String: AnyObject] {
-      //  var info : [String: AnyObject] = [String: AnyObject]()
-        
-        let jsonObject: [String: AnyObject] = ["bUser": true, "ID": 21, "name": "Ted", "curLoc": [0.3, 0.0] ]
-        let driverObject: [String: AnyObject] = ["bUser": false, "ID": 52, "name": "Mike", "curLoc": [0.3, 0.0] ]
         let driverObject1: [String: AnyObject] = ["bUser": false, "ID": 53, "name": "Philips", "curLoc": [0.30001, 0.0] ]
-        
-        let jsonObject1: [String: AnyObject] = ["ID": 21, "fromLoc": [0.3, 0.0], "toLoc": [0.3, 0.0], "requestProduct": "shit", "payment": 0.1, "passcode":"1234"]
-        let jsonObject2: [String: AnyObject] = ["ID": 21]
-        let jsonObject3: [String: AnyObject] = ["userID": 21, "driverID": 53]
         let jsonObject4: [String: AnyObject] = ["driverID": 53]
         
         var handeler = RequestHandler()
-        //clear database.
-       // handeler.sendRequestByURL(jsonObject, tag: "clearAllDataBase")
-        /*
-        handeler.sendRequestByURL(jsonObject, tag: "addPerson")
-        handeler.sendRequestByURL(driverObject, tag: "addPerson")
-        handeler.sendRequestByURL(driverObject1, tag: "addPerson")
-        handeler.sendRequestByURL(jsonObject1, tag: "submitUserDeliveryForm")
-        handeler.sendRequestByURL(jsonObject2, tag: "listNearbyDrivers")
-        handeler.sendRequestByURL(jsonObject3, tag: "selectDriver")
-        info = handeler.sendRequestByURL(jsonObject4, tag: "pollUserRequest")
-        println(info)
-        */
         handeler.sendRequestByURL(driverObject1, tag: "addPerson")
         var info: [String: AnyObject] = handeler.sendRequestByURL(jsonObject4, tag: "pollUserRequest")!
+        
+        // Keep on polling user request
         while ( (info["status"] as AnyObject? as? Int) == -1) {
             info = handeler.sendRequestByURL(jsonObject4, tag: "pollUserRequest")!
             sleep(2)
